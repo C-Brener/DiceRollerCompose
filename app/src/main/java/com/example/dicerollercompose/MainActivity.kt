228b84e687288e085e0c8f9abaaad25999102193
Caíque Brener <brenercaique0806@gmail.com>
2022-12-11 12:06:05 -0300

    Finish Dice roller

--- app/src/main/java/com/example/dicerollercompose/MainActivity.kt
+++ app/src/main/java/com/example/dicerollercompose/MainActivity.kt
@@ -3,20 +3,19 @@ package com.example.dicerollercompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.dicerollercompose.ui.theme.DiceRollerComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -39,10 +38,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
-    Column(modifier=modifier, horizontalAlignment = Alignment.CenterHorizontally) {
-        Button(onClick = { /*TODO*/ }) {
+    var result: Int by remember { mutableStateOf(1) }
+    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(painter = painterResource(id = getImageDice(result) ), contentDescription = null)
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { result = (1..6).random() }) {
             Text(text = stringResource(R.string.roll))
-            
         }
     }
 }
@@ -50,7 +51,33 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
 @Preview(showSystemUi = true)
 @Composable
 fun DiceWithButtonAndImagePreview() {
-    DiceWithButtonAndImage(modifier = Modifier
-        .fillMaxSize()
-        .background(Color.Black))
+    DiceWithButtonAndImage(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black)
+    )
+}
+
+private fun getImageDice(result:Int): Int {
+    return when (result) {
+        1 -> {
+            R.drawable.dice_1
+        }
+        2 -> {
+            R.drawable.dice_2
+        }
+        3 -> {
+            R.drawable.dice_3
+        }
+        4 -> {
+            R.drawable.dice_4
+        }
+        5 -> {
+            R.drawable.dice_5
+        }
+        else -> {
+            R.drawable.dice_6
+        }
+    }
+
 }
